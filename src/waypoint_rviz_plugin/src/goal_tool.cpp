320586e2537775f264b690494f9f41e3d7ab5b7b
Bottle101 <hebotao101@gmail.com>
2024-04-28 18:38:05 -0400

    3D goal finished

--- src/waypoint_rviz_plugin/src/goal_tool.cpp
+++ src/waypoint_rviz_plugin/src/goal_tool.cpp
@@ -30,11 +30,10 @@
 #include <tf/transform_listener.h>
 
 #include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/Joy.h>
 
 #include "rviz/display_context.h"
 #include "rviz/properties/string_property.h"
-
-#include <quadrotor_msgs/GoalSet.h>
 
 #include "goal_tool.h"
 
@@ -56,36 +55,57 @@ Goal3DTool::Goal3DTool()
 void Goal3DTool::onInitialize()
 {
   Pose3DTool::onInitialize();
-  setName("3D Nav Goal");
+  setName("Waypoint3D");
   updateTopic();
 }
 
 void Goal3DTool::updateTopic()
 {
-  pub_goal_ = nh_.advertise<geometry_msgs::PoseStamped>(topic_property_->getStdString(), 1);
-  pub_droneID_goal_ = nh_.advertise<quadrotor_msgs::GoalSet>("/goal_with_id", 1);
+  wp_pub_ = nh_.advertise<geometry_msgs::PointStamped>("/way_point", 5);
+  pub_joy_ = nh_.advertise<sensor_msgs::Joy>("/joy", 5);
 }
 
 void Goal3DTool::onPoseSet(double x, double y, double z, double theta)
 {
-  ROS_WARN("3D Goal Set");
-  std::string fixed_frame = context_->getFixedFrame().toStdString();
-  tf::Quaternion quat;
-  quat.setRPY(0.0, 0.0, theta);
-  tf::Stamped<tf::Pose> p = tf::Stamped<tf::Pose>(tf::Pose(quat, tf::Point(x, y, z)), ros::Time::now(), fixed_frame);
-  geometry_msgs::PoseStamped goal;
-  tf::poseStampedTFToMsg(p, goal);
-  ROS_INFO("Setting goal: Frame:%s, Position(%.3f, %.3f, %.3f), Orientation(%.3f, %.3f, %.3f, %.3f) = Angle: %.3f\n", fixed_frame.c_str(),
-           goal.pose.position.x, goal.pose.position.y, goal.pose.position.z,
-           goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w, theta);
-  pub_goal_.publish(goal);
+  // ROS_WARN("3D Goal Set");
 
-  quadrotor_msgs::GoalSet goal_with_id;
-  goal_with_id.drone_id = 0;
-  goal_with_id.goal[0] = x;
-  goal_with_id.goal[1] = y;
-  goal_with_id.goal[2] = z;
-  pub_droneID_goal_.publish(goal_with_id);
+  sensor_msgs::Joy joy;
+
+  joy.axes.push_back(0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(-1.0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(1.0);
+  joy.axes.push_back(1.0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(0);
+
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(1);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+
+  joy.header.stamp = ros::Time::now();
+  joy.header.frame_id = "waypoint_tool";
+  pub_joy_.publish(joy);
+
+  geometry_msgs::PointStamped waypoint;
+  waypoint.header.frame_id = "map";
+  waypoint.header.stamp = joy.header.stamp;
+  waypoint.point.x = x;
+  waypoint.point.y = y;
+  waypoint.point.z = z;
+
+  wp_pub_.publish(waypoint);
+  usleep(10000);
+  wp_pub_.publish(waypoint);
 }
 
 } // end namespace rviz
