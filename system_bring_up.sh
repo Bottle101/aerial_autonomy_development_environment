a11365388300c5ac72ad18385ca6aab3a8797b23
Bottle101 <hebotao101@gmail.com>
2024-04-29 18:12:06 -0400

    Added log folder, removed redundant files

--- system_bring_up.sh
+++ system_bring_up.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+ENV="factory_low_resolution" #factory, villege, urban_city, town, old_town, office1, office2
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 
 cd $SCRIPT_DIR
 source ./devel/setup.bash
-./src/vehicle_simulator/mesh/factory_low_resolution/environment/factory_low_resolution.x86_64 &
+./src/vehicle_simulator/mesh/"$ENV"/environment/"$ENV".x86_64 &
 roslaunch vehicle_simulator system_unity.launch
