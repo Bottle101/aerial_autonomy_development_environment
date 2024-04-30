43eb859c1ce64f813eee2cf770088743366f1db5
Bottle101 <hebotao101@gmail.com>
2024-04-30 15:11:00 -0400

    Modified launch method for unity

--- system_bring_up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ENV="factory" #factory, villege, urban_city, town, old_town, office1, office2
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-
-cd $SCRIPT_DIR
-source ./devel/setup.bash
-./src/vehicle_simulator/mesh/"$ENV"/environment/"$ENV".x86_64 &
-roslaunch vehicle_simulator system_unity.launch
