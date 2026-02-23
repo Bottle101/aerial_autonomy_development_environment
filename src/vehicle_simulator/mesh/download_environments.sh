a2dba0a55ab37e57f0e9d41356f581a95934e1d8
Botao He <hebotao101@gmail.com>
2026-02-22 20:54:26 -0800

    Update Google Drive ID for environment download script

--- src/vehicle_simulator/mesh/download_environments.sh
+++ src/vehicle_simulator/mesh/download_environments.sh
@@ -6,7 +6,7 @@ echo ""
 echo "Downloading simulation environments..."
 echo ""
 
-ggID='1GMT8tptb3nAb87F8eFfmIgjma6Bu0reV'
+ggID='1rbz8oPKPClhJVZZcL3R-h6qNaxGA3_5D'
 ggURL='https://drive.usercontent.google.com/download'
 
 filename="$(curl -sc /tmp/gcokie "${ggURL}?id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
